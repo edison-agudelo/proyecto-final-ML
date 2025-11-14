d28975fd6c97c549622864557c67eec6a6984d75
Juan David Orduña Vargas <jorduna@ucundinamarca.edu.co>
2025-11-14 12:56:56 -0500

    Add MYSQL_PORT from env

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', '')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'pure_ml')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 3306))
+
 mysql = MySQL(app)
 
 UPLOAD_FOLDER = os.path.join('static', 'img')
